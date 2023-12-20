4332bfab0c7ea49985890a427ed3dcec0dab7818
Gwen Nicodemus <tamaragwen@gmail.com>
2023-12-19 21:59:14 -0500

    added nose arguments

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,6 +8,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.8 python3.8-venv
 
 echo "Making Python 3.8 the default..."
+sudo update-alternatives --remove-all python3
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
 
